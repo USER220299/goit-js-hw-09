d3150a132b1032c1d13260baad3f55cebfef9f24
USER220299 <127625631+USER220299@users.noreply.github.com>
2023-08-01 22:01:54 +0300

    solution 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,28 +1,54 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-flatpickr("#datetime-picker", {
-    enableTime: true,
+
+const input = document.querySelector("#datetime-picker");
+console.log(input);
+const startBtn = document.querySelector('button[data-start]');
+console.log(startBtn);
+
+startBtn.setAttribute('disabled', "");
+
+const options = {
+  enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
-  },
-
-})
-const input = document.querySelector("#datetime-picker");
-console.log(input);
-const startBtn = document.querySelector('button[data-start]');
-
-console.log(startBtn);
-
-input.addEventListener('input', onChoiseDate);
-const date = new Date();
-function onChoiseDate() {
-    if (date !== onClose()) {
-       window.alert("Please choose a date in the future")
+    if (new Date() > selectedDates[0] ) {
+     window.alert("Please choose a date in the future");
+     
+  } else {
+    startBtn.removeAttribute('disabled', "");
     }
-    
+  }
+
+};
+
+flatpickr("#datetime-picker", options);
+
+
+startBtn.addEventListener('click', convertMs);
+
+function convertMs(ms) {
+  const currentDate = new Date();
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
 
-console.dir(date)
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
